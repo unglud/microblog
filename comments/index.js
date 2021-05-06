1b5c65beb89f2ec1f38c399a2789d143e0037cd9
Aleksandr Matrosov <unglud@gmail.com>
2021-05-06 11:18:39 +0200

    emit events

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const { post } = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,13 +13,18 @@ const commentsByPostId = {};
 app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+
+  await post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: { id: commentId, content, postId: req.params.id },
+  });
 
   res.status(201).send(comments);
 });
