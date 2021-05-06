80a761882d20e3930152b07b60cdecc053d559fd
Aleksandr Matrosov <unglud@gmail.com>
2021-05-06 11:41:41 +0200

    fetching data from query service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,22 +1,6 @@
-import {useEffect, useState} from "react";
-import axios from "axios";
-
-export default ({postId}) => {
-    const [comments, setComments] = useState([]);
-
-    const fetchData = async () => {
-        const res = await axios.get(
-            `http://localhost:4001/posts/${postId}/comments`
-        );
-        setComments(res.data);
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const renderedComments = comments.map((comment) => {
-        return <li key={comment.id}>{comment.content}</li>;
-    });
-    return <ul>{renderedComments}</ul>;
+export default ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+  return <ul>{renderedComments}</ul>;
 };
