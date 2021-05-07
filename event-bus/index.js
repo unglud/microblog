e671e511a4a3608a1c125ad10a0ff20a01d389f7
Aleksandr Matrosov <unglud@gmail.com>
2021-05-07 10:27:26 +0200

    handle past events

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const { post } = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+
+const events = [];
 
 app.post("/events", (req, res) => {
   const event = req.body;
+  events.push(event);
 
   post("http://localhost:4000/events", event);
   post("http://localhost:4001/events", event);
@@ -16,6 +17,10 @@ app.post("/events", (req, res) => {
   post("http://localhost:4003/events", event);
 
   res.send({ status: "ok" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
