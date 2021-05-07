6da7a650cfc174f447a46b9530ad4f013c455130
Aleksandr Matrosov <unglud@gmail.com>
2021-05-07 09:58:45 +0200

    update content

--- comments/index.js
+++ comments/index.js
@@ -30,8 +30,24 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  console.log(`Recevied Event`, req.body.type);
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentsModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: { id, content, postId, status },
+    });
+  }
 
   res.send({ status: "ok" });
 });
