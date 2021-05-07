a66b290d520cb30e1957779ff333e1d884c48fb0
Aleksandr Matrosov <unglud@gmail.com>
2021-05-07 10:37:55 +0200

    handle past events

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,10 +11,18 @@ app.post("/events", (req, res) => {
   const event = req.body;
   events.push(event);
 
-  post("http://localhost:4000/events", event);
-  post("http://localhost:4001/events", event);
-  post("http://localhost:4002/events", event);
-  post("http://localhost:4003/events", event);
+  post("http://localhost:4000/events", event).catch((error) =>
+    console.log(error.message)
+  );
+  post("http://localhost:4001/events", event).catch((error) =>
+    console.log(error.message)
+  );
+  post("http://localhost:4002/events", event).catch((error) =>
+    console.log(error.message)
+  );
+  post("http://localhost:4003/events", event).catch((error) =>
+    console.log(error.message)
+  );
 
   res.send({ status: "ok" });
 });
